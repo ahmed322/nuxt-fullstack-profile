f38ea1a3a7cd650e1658d89f7bd8401a941be0f5
ahmed kamel <skownx9@gmail.com>
2024-11-13 19:26:15 +0200

    provide basic seo tags

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+useHead({
+	link: [
+		{
+			rel: "icon",
+			type: "image/x-icon",
+			href: "/favicon.ico",
+		},
+	],
+	titleTemplate: (titleChunck) => {
+		return titleChunck ? `${titleChunck} - Ahmed-portfolio` : `Ahmed-portfolio`;
+	},
+});
+</script>
 <template>
 	<div>
 		<NuxtRouteAnnouncer />
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+useHead({
+	htmlAttrs: {
+		lang: "en",
+	},
+	title: "About",
+	meta: [
+		{
+			name: "description",
+			content:
+				"I’m Ahmed, a full-stack developer with a focus on building both intuitive front-end interfaces and efficient back-end systems. My expertise spans HTML, CSS, JavaScript (JS), and TypeScript (TS), along with responsive design using Bootstrap 5 and Tailwind CSS. I enjoy creating visually appealing and user-friendly applications, especially using Vue.js and Nuxt (including TypeScript integration) to develop dynamic SPAs and SSR apps.On the back end, I work with Fastify.js to deliver fast, scalable server-side applications, often integrating with MongoDB and Mongoose for handling data. My approach prioritizes clean, maintainable code and best practices in both front-end and back-end development.",
+		},
+	],
+});
+</script>
 <template>
 	<div>About:page</div>
 </template>
